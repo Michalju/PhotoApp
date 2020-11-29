23e7a7b9953520bf4119102829d047c55179c929
Michalju <juszkin@gmail.com>
2020-11-29 11:52:31 +0000

    added add country html page. CSS to be added later.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from controllers.locations_controller import locations_blueprint
+from controllers.countries_controller import countries_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(locations_blueprint)
+app.register_blueprint(countries_blueprint)
 
 @app.route("/")
 def main():
---
+++ controllers/countries_controller.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.country import Country
+import repositories.country_repository as country_repository
+import repositories.continent_repository as continent_repository
+
+countries_blueprint = Blueprint("countries", __name__)
+
+@countries_blueprint.route("/countries/add")
+def countries_new():
+    continents = continent_repository.select_all()
+    return render_template("countries/add.html", continents=continents, title="Add a country")
+
+@countries_blueprint.route("/countries/add", method=['POST'])
+def countries_add():
+    new_country = Country(request.form['name'], continent_repository.select(request.form['continent_id']))
+    country_repository.save(new_country)
+
+    return redirect('/')
